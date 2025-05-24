7c9adbf102d37b96759df773a3741ae90be330dd
yoonaoh312 <yoonaoh312@gmail.com>
2025-05-24 09:59:21 -0600

    score file r/a/w

--- chapters/chapter8-1.py
+++ chapters/chapter8-1.py
@@ -18,11 +18,27 @@
 
 # always save as string
 
-print('{0:>10}'.format(500))
-print('{0:>+10}'.format(500))
-print('{0:>+10}'.format(-500))
-print('{0:-<10}'.format(500))
-print('{0:,}'.format(10000000000))
-print('{0:+,}'.format(10000000000))
-print('{0:+,}'.format(-10000000000))
-print('{0:^<+30,}'.format(10000000000000000))
+# print('{0:>10}'.format(500))
+# print('{0:>+10}'.format(500))
+# print('{0:>+10}'.format(-500))
+# print('{0:-<10}'.format(500))
+# print('{0:,}'.format(10000000000))
+# print('{0:+,}'.format(10000000000))
+# print('{0:+,}'.format(-10000000000))
+# print('{0:^<+30,}'.format(10000000000000000))
+# print('{0:f}'.format(5/3))
+# print('{0:.2f}'.format(5/3))
+
+# score_file = open('score.txt', 'w', encoding='utf8')
+# print('math: 0', file=score_file)
+# print('english: 50', file=score_file)
+# score_file.close()
+
+# score_file = open('score.txt', 'a', encoding='utf8')
+# score_file.write('science: 80')
+# score_file.write('\ncoding: 100')
+# score_file.close()
+
+score_file = open('score.txt', 'r', encoding='utf8')
+print(score_file.read())
+score_file.close()
