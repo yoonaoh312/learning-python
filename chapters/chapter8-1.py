c5c97092d44b03a86de637f3e4adfa370ee1ecf3
yoonaoh312 <yoonaoh312@gmail.com>
2025-05-28 16:21:01 -0600

    how to read lines from txt file

--- chapters/chapter8-1.py
+++ chapters/chapter8-1.py
@@ -16,7 +16,7 @@
 # answer = input('insert anything: ') 
 # print('you inserted that '+answer)
 
-# always save as string
+# # always save as string
 
 # print('{0:>10}'.format(500))
 # print('{0:>+10}'.format(500))
@@ -29,16 +29,31 @@
 # print('{0:f}'.format(5/3))
 # print('{0:.2f}'.format(5/3))
 
-# score_file = open('score.txt', 'w', encoding='utf8')
-# print('math: 0', file=score_file)
-# print('english: 50', file=score_file)
-# score_file.close()
+score_file = open('score.txt', 'w', encoding='utf8')
+print('math: 0', file=score_file)
+print('english: 50', file=score_file)
+score_file.close()
 
-# score_file = open('score.txt', 'a', encoding='utf8')
-# score_file.write('science: 80')
-# score_file.write('\ncoding: 100')
-# score_file.close()
+score_file = open('score.txt', 'a', encoding='utf8')
+score_file.write('science: 80')
+score_file.write('\ncoding: 100')
+score_file.close()
 
 score_file = open('score.txt', 'r', encoding='utf8')
 print(score_file.read())
+score_file.close()
+
+score_file = open('score.txt', 'r', encoding='utf8')
+print(score_file.readline(), end="") #reading a line by line
+print(score_file.readline())
+print(score_file.readline())
+print(score_file.readline())
+score_file.close()
+
+score_file = open("score.txt", "r", encoding="utf8")
+while True:
+    line = score_file.readline()
+    if not line:
+        break
+    print(line, end="")
 score_file.close()
